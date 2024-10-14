f9e09d083f857e71da7cf915253dfe2d2135ca17
Umar Ejaz <johndoe@example.com~>
2024-10-14 03:50:42 -0700

    v1.12

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Navbar from "./components/Navbar";
 import HomePages from "./Pages/HomePages";
 import Body from "./components/Body";
 import "../src/App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <>
       <Body />
+      <Analytics />
     </>
   );
 };
