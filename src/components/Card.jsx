c49a4bf42035dc611cbbf9d3e3238faba343d584
umar ejaz <umar.senpaiiii@gmail.com>
2025-02-14 16:04:41 +0500

    v7

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
-const Card = ({key, data }) => {
+const Card = ({ key, data }) => {
   const { imgUrl, title, description, tags, liveLink } = data;
   return (
-    <div  className="w-full sm:w-[360px] bg-zinc-800 text-white rounded-xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
+    <div className="w-full sm:w-[360px] bg-zinc-800 text-white rounded-xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
       <img src={imgUrl} alt={title} className="w-full h-48 object-cover" />
       <div className="p-5">
         <h3 className="text-xl font-bold mb-2">{title}</h3>
@@ -26,7 +26,6 @@ const Card = ({key, data }) => {
             <i className="fas fa-external-link-alt"></i>
             Live Demo
           </a>
-    
         </div>
       </div>
     </div>
